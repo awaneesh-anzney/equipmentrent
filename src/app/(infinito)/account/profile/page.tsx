80d4a37d83dc84e18953f6c3c6979ac97891eb73
iam-awaneesh <aw64an@gmail.com>
2026-02-22 18:21:30 +0530

    feat: implement user profile management page and component.

---
+++ src/app/(infinito)/account/profile/page.tsx
@@ -0,0 +1,12 @@
+import Profile from "@/components/profile/Profile";
+import React from "react";
+
+const page = () => {
+    return (
+        <div className="min-h-screen bg-background pt-24 pb-20">
+            <Profile />
+        </div>
+    );
+};
+
+export default page;
